513b29818c393a01472618ea87e4ba067e21a965
bahamuru <74848330+bahamuru@users.noreply.github.com>
2020-11-22 15:05:57 +0900

    edit for led

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 async function main() {
   const button = document.getElementById("button");
   const ledView = document.getElementById("ledView");
+  const gpioAccess = await navigator.requestGPIOAccess();
+  const ledPort = gpioAccess.ports.get(26); // LED の GPIO ポート番号
+  await ledPort.export("out");
 
   let lit = false;
   button.onclick = async function () {
@@ -8,7 +11,7 @@ async function main() {
 
     const color = lit ? "red" : "black";
     ledView.style.backgroundColor = color;
-  };
+
 }
 
 main();
